84ea97b7bec1ed008805dd1e938a214e0c32fe25
HARSHENI <32952480+HARSHENI@users.noreply.github.com>
2017-10-20 16:14:22 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse011
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse011 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=5
+b=25
+e=a+b
+f=a-b
+c=a%b
+d=a/b
+g=b**a
+print(c)
+print(d)
+print(f)
+print(g)
+print(e)
