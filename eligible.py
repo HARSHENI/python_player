e4ae8dc12a3cc885515fd40c58fc97a056ead8d9
HARSHENI <32952480+HARSHENI@users.noreply.github.com>
2017-10-21 10:28:54 +0530

    Add files via upload

---
+++ eligible.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse011
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse011 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+a=17
+if(a>=20):
+    print("a is eligible")
+else:
+    print("not eligible")
