be9529c080dd9f800b0a47c7cf549356a58f5587
HARSHENI <32952480+HARSHENI@users.noreply.github.com>
2017-10-21 12:16:48 +0530

    Add files via upload

---
+++ greatest order.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse011
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse011 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+def my_function():
+    print "Hello From My Function!"
+
+def my_function_with_args(first,sec):
+    print first + sec
+
+
+my_function()
+my_function_with_args("Your name ", "have a great year!")
