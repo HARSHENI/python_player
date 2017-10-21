319978e01736d83bf642a710a97c530da6682053
HARSHENI <32952480+HARSHENI@users.noreply.github.com>
2017-10-21 09:46:59 +0530

    Add files via upload

---
+++ place of r,g.p.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse011
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse011 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+mystring='programmer'
+print(mystring.find("r"))
+print(mystring.find("g"))
